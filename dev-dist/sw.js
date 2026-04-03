8dddbfa2c8a96c9fc451724c00adbd94a9ef1ef7
Pooja Lahane <pooja.lahane.gits@gmail.com>
2026-04-03 15:47:45 +0530

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p62inm3kn9k"
+    "revision": "0.79mtj8hmcig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
